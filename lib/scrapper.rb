b161e07f751e970fe84012f88a072ac0dbd20699
Rashad-Muntar <muntartouregmail.com>
2021-03-09 00:24:44 -0800

    made property method private

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -4,15 +4,9 @@ require 'byebug'
 require 'colorize'
 
 class Scrapper
-  attr_reader :parsed_data
-
   def initialize
     doc = HTTParty.get('https://tonaton.com/en/ads/ghana/property?sort=date&buy_now=0&urgent=0&page=1')
     @parsed_data ||= Nokogiri::HTML(doc.body)
-  end
-
-  def property
-    parsed_data.css('ul.list--3NxGO > li > a > div > div > h2').map { |property| property.text.upcase }
   end
 
   def data_list
@@ -21,8 +15,12 @@ class Scrapper
 
   private
 
+  def property
+    @parsed_data.css('ul.list--3NxGO > li > a > div > div > h2').map { |property| property.text.upcase }
+  end
+
   def price
-    parsed_data.css('div.content--3JNQz > div > .price--3SnqI').map do |price|
+    @parsed_data.css('div.content--3JNQz > div > .price--3SnqI').map do |price|
       price.text.upcase
     end
   end
--- spec/scrapper_spec.rb
+++ spec/scrapper_spec.rb
@@ -1,24 +1,6 @@
 require_relative '../lib/scrapper'
 
 describe Scrapper do
-  # describe '#price' do
-  #   it 'Returns a array containing contents of elements from the NodeSets given' do
-  #     scrapper = Scrapper.new
-  #     expect(scrapper.price).to be_a Array
-  #   end
-  # end
-
-  describe '#property' do
-    it 'return an array of strings' do
-      scrapper = Scrapper.new
-      expect(scrapper.property).to be_a Array
-    end
-    it 'do not return a hash object data type' do
-      scrapper = Scrapper.new
-      expect(scrapper.property).to_not be_an Hash
-    end
-  end
-
   describe '#data_list' do
     it 'returns a hash of all available property listing' do
       scrapper = Scrapper.new
